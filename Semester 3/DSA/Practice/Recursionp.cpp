16eefd46f27686d965590bf340644b7abe856981
SalmanSaeed-1 <salmansaeedch008@gmail.com>
2026-09-16 20:34:35 +0500

    Basic practice and testing

---
+++ Semester 3/DSA/Practice/Recursionp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return 1 + countDigits(n / 10);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Number of digits = " << countDigits(n);
+
+    return 0;
+}
